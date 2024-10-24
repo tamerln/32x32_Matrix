624c9d507b76f3d493052b785a2d9ad7837debf3
tamerln <benebin@protonmail.com>
2024-10-23 21:02:37 -0600

    Fixed edge cases

---
+++ GOL/GOL.ino
@@ -0,0 +1,111 @@
+#include <Adafruit_Protomatter.h>
+
+Adafruit_Protomatter matrix(
+  32,          // Width of matrix (or matrix chain) in pixels
+  4,           // Bit depth, 1-6
+  1, rgbPins,  // # of matrix chains, array of 6 RGB pins for each
+  4, addrPins, // # of address pins (height is inferred), array of pins
+  clockPin, latchPin, oePin, // Other matrix control pins
+  false);      // No double-buffering here (see "doublebuffer" example)
+
+int gameWidth = 32;
+int gameHeight = 32;
+int Board[gameHeight][gameWidth] = {0};
+
+
+void updateMatrix();
+void doTurn();
+void launchGlider();
+int getNeighbors(int row, int column);
+
+void setup(void) {
+  Serial.begin(9600);
+
+  // Initialize matrix...
+  ProtomatterStatus status = matrix.begin();
+  Serial.print("Protomatter begin() status: ");
+  Serial.println((int)status);
+  if(status != PROTOMATTER_OK) {
+    // DO NOT CONTINUE if matrix setup encountered an error.
+    for(;;);
+  }
+}
+
+void loop(void) {
+
+  launchGliders();
+
+  for (int i = 0; i < 15; i++) {
+  doTurn();
+  updateMatrix();
+  matrix.show();
+  delay(20);
+  }
+  
+  
+  
+}   
+
+void doTurn() {
+    int numNeighbors;
+    int tempBoard[gameHeight][gameWidth] = {0};
+
+    for (int i = 0; i < gameHeight; i++) { //row
+        for(int j = 0; j < gameWidth; j++) { //column
+            numNeighbors = getNeighbors(i, j);
+            //death (neighbors < 2 || neighbors > 3)
+            if (Board[i][j] == 1) { // Alive
+                if (numNeighbors == 2 || numNeighbors == 3) {
+                  tempBoard[i][j] = 1; 
+                } 
+            } else { // Dead
+                if (numNeighbors == 3) {
+                    tempBoard[i][j] = 1;
+                } //if cell either doesnt stay alive or become alive, it will be a zero on tempBoard already
+            }
+          }
+        }
+          memcpy(Board, tempBoard, sizeof(Board));  //copy tempBoard to Board 
+      }
+   
+
+  
+int getNeighbors(int row, int column) { 
+ int neighbors = 0;
+    for (int r = row - 1; r <= row + 1; r++) {
+        for (int c = column - 1; c <= column + 1; c++) {
+            if (r == row && c == column) continue; //skip the cell itself
+            if ((r > 31 || r < 0) || (c > 31 || c < 0)) continue; //if out of bounds count it dead and continue iterating
+            if (Board[r][c] == 1) neighbors++; //if current cell is alive, add 1 to neighbors count
+        }
+    }
+    return neighbors;
+}
+
+void updateMatrix() {
+        for (int i = 0; i < gameHeight; i++) { //row
+              for(int j = 0; j < gameWidth; j++) { //column
+                  if (Board[i][j] == 1) {
+                  matrix.drawPixel(j, i, matrix.color565(0, 255, 0)); ;
+                 } else {
+                  matrix.drawPixel(j, i, matrix.color565(0, 0, 0));
+                 }
+              }
+        }
+}
+
+void launchGliders() { //initializes  2 gosper gliders from top corners of matrix
+  Board[0][1] = 1;
+  Board[1][2] = 1;
+  Board[2][0] = 1;
+  Board[2][1] = 1;
+  Board[2][2] = 1;
+
+  Board[0][31] = 1;
+  Board[1][30] = 1;
+  Board[2][30] = 1;
+  Board[1][29] = 1;
+  Board[0][29] = 1;
+
+}
+
